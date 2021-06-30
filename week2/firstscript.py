f365b755d2762af3d81f2dd5a53298273ca00cd7
Jesse McNaught <jmcnaught@madisoncollege.edu>
2021-06-29 20:10:42 -0500

    My First Script-FIXED

---
+++ week2/firstscript.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Prints out a name and one thing learned.
+
+print("Jesse McNaught")
+print("I learned how to do my first script! Can't wait to see more, this stuff is cool!")
